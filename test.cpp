bdab5b8acb0778dbe4426ae01147ae5eb7a9e96b
Dan Ni <61080530+Danni4real@users.noreply.github.com>
2025-01-16 10:10:55 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,44 +1,44 @@
 #include <thread>
+#include <string>
 
 #include "ThreadLog.h"
 
 void call_3()
 {
-	TRACK_CALL("no arg");
+	TRACK_CALL("");
 	{
-		TRACK("handle call sleep","%d",1);
+		TRACK("sleep","%d",1);
 		sleep(1);
 	}
 	LOG_INFO("this is call_3");
 }
 
-void call_2()
+void call_2(int a, float b)
 {
-	TRACK_CALL();
+	TRACK_CALL("a:%d, b:%.1f",a,b);
 	sleep(1);
 	call_3();
 }
 
-void call_1()
+void call_1(int a, float b, std::string c)
 {
-	TRACK_CALL();
+	TRACK_CALL_X(a, b, c);
 	sleep(1);
-	call_2();
+	call_2(a,b);
 }
 
 int main()
 {
-	
 	std::thread t_1 = std::thread([&]{
-		call_1();
+		call_1(1,2.0,"three");
 	});
 	
 	std::thread t_2 = std::thread([&]{
-		call_1();
+		call_1(2,3.0,"four");
 	});
 	
 	std::thread t_3 = std::thread([&]{
-		call_1();
+		call_1(3,4.0,"five");
 	});
 	
 	t_1.join();
